6a3e666ca6f6d363c130b308a2a0b159b768fc2d
Peter Meister <prmeister89@gmail.com>
2018-07-09 14:49:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
   
   if new_array.length == 1 
     return new_array
-  elsif new_arrayarray.length == 2
+  elsif new_array.length == 2
     return new_array.split(" and ")
-  elsif array.length == 3
+  elsif new_array.length == 3
     3_array = []
     3_array << 
   
