02bbb254e277fcf91cd89553a648b9269bd79848
Peter Meister <prmeister89@gmail.com>
2018-07-09 15:06:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   
   if array.length >== 3
     new_array = []
-    new_array << array.insert(-2," and ")
+    new_array << array + array.insert(-2," and ")
     return array.join(",") + new_array
   elsif array.length == 2
     return array.join(" and ")
