83d80d864a725d22ca1cecda3b23933a5c3db7f7
Peter Meister <prmeister89@gmail.com>
2018-07-09 15:34:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   if array.length >= 3
     new_array = []
     binding.pry
-    new_array << array[0..-2] + "and #{array[-1]}"
+    new_array << array[0..-2]
+    new_array.push("and #{array[-1]}")
     
     
     return new_array.join(",")
