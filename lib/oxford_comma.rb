2d27b45d07e2d0819062756596c8621a60fac4b0
Peter Meister <prmeister89@gmail.com>
2018-07-09 14:57:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,11 @@ def oxford_comma(array)
   if array.length == 1 
     return array.join
   elsif array.length == 2
-    return array.split(" and ")
+    return array.join(" and ")
   elsif array.length == 3
     new_array = []
-    new_array << 
+    new_array << array[-1]
+    return array.join(",") + new_array.join(" and ")
   
   
 end
