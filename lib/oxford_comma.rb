6d0c2b43a85ea5595fadbc8b03dce2274a1aa900
Peter Meister <prmeister89@gmail.com>
2018-07-09 15:20:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ require 'pry'
 def oxford_comma(array)
   
   if array.length >= 3
-    array.insert(-2," and ")
-    binding.pry
+    #array.insert(-2," and ")
+   # binding.pry
     return array.join(",")
   elsif array.length == 2
     return array.join(" and ")
