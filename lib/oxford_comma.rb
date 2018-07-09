c56d1914ca3a409b8c65360c0af584a7b22d27a6
Peter Meister <prmeister89@gmail.com>
2018-07-09 14:46:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
-    return array
+    return array.to_a
   elsif array.length == 2
     return array.split(" and ")
   elsif array.length == 3
-    return array.
+    new_array = []
+    new_array << 
   
   
 end
