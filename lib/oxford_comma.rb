6b319588180ee3b83e6a9d8e8097b6729eef4c06
Peter Meister <prmeister89@gmail.com>
2018-07-09 14:59:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
-  elsif array.length == 3
+  elsif array.length =<= 3
     new_array = []
     new_array << array[-1]
     return array.join(",") + new_array.join(" and ")
