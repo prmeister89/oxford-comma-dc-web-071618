616fa2ed07821b79f2137601c89a2065059863ee
Peter Meister <prmeister89@gmail.com>
2018-07-09 15:03:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,5 @@
 def oxford_comma(array)
   
-  if array.length == 1 
-    return array.join
-  elsif array.length == 2
-    return array.join(" and ")
-  else 
-    array.length =<= 3
-    new_array = []
-    new_array << array.insert(-2," and ")
-    return array.join(",") + new_array
-  end
-end
-
-
-def oxford_comma(array)
-  
   if array.length =<= 3
     new_array = []
     new_array << array.insert(-2," and ")
