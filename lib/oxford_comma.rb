1eb78db888a2639532cabade6092630bc473dd74
Peter Meister <prmeister89@gmail.com>
2018-07-09 14:55:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   
   if array.length == 1 
-    return array.join("")
+    return array.join
   elsif array.length == 2
     return array.split(" and ")
   elsif array.length == 3
