fef3811fc92e1788321518a66ed358a31bb51028
Peter Meister <prmeister89@gmail.com>
2018-07-09 14:54:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  array.split("")
+  
   if array.length == 1 
-    return array.to_a
+    return array.split("")
   elsif array.length == 2
     return array.split(" and ")
   elsif array.length == 3
